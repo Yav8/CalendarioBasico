6f097349aa312339a58dc48d00cbe0af7812b61e
Yav <yavieron@hotmail.es>
2017-11-01 02:01:01 +0100

    Creada clase calendario que inicializa objetos con fecha 01-01-01

---
+++ CalendarioBasico.java
@@ -0,0 +1,24 @@
+
+/**
+ * CalendarioBásico
+ *
+ * @autor (Javier de Cea Domínguez)
+ * @version (31-10-2017)
+ */
+public class CalendarioBasico {
+    // Recoge el día.
+    private int dia;
+    // Recoge el mes.
+    private int mes;
+    // Recoge el año.
+    private int ano;
+
+    /**
+     * Inicializa objetos con la siguiente fecha: día 01, mes 01, año 01.
+     */
+    public CalendarioBasico() {
+        dia = 1;
+        mes = 1;
+        ano = 1;
+    }
+}
